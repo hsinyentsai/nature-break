86911ec249c28a97d6046616ce17e1c307ef1c83
hsinyentsai <hytsai@seas.upenn.edu>
2025-01-25 21:47:59 -0500

    icons issue

--- nature-walk/src/Components/MusicMenu.js
+++ nature-walk/src/Components/MusicMenu.js
@@ -9,22 +9,22 @@ export const MusicMenu = ({ currentScene }) => {
 
   const sceneTracks = {
     Forest: [
-      { id: 1, icon: <img src="/icons/sun.png" alt="Sunny" className="weather-icon" />, src: '/Asset/forest-sunny.mp3' },
-      { id: 2, icon: <img src="/icons/wind.png" alt="Windy" className="weather-icon" />, src: '/Asset/forest-windy.mp3' },
-      { id: 3, icon: <img src="/icons/rain.png" alt="Rainy" className="weather-icon" />, src: '/Asset/forest-rainy.mp3' },
-      { id: 4, icon: <img src="/icons/snow.png" alt="Snowy" className="weather-icon" />, src: '/Asset/forest-snowy.mp3' },
+      { id: 1, icon: <img src={`${process.env.PUBLIC_URL}/icons/sun.png`} alt="Sunny" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/forest-sunny.mp3` },
+      { id: 2, icon: <img src={`${process.env.PUBLIC_URL}/icons/wind.png`} alt="Windy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/forest-windy.mp3` },
+      { id: 3, icon: <img src={`${process.env.PUBLIC_URL}/icons/rain.png`} alt="Rainy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/forest-rainy.mp3` },
+      { id: 4, icon: <img src={`${process.env.PUBLIC_URL}/icons/snow.png`} alt="Snowy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/forest-snowy.mp3` },
     ],
     Ocean: [
-      { id: 1, icon: <img src="/icons/sun.png" alt="Sunny" className="weather-icon" />, src: '/Asset/sunny-beach.mp3' },
-      { id: 2, icon: <img src="/icons/wind.png" alt="Windy" className="weather-icon" />, src: '/Asset/windy-beach.mp3' },
-      { id: 3, icon: <img src="/icons/rain.png" alt="Rainy" className="weather-icon" />, src: '/Asset/rainy-beach.mp3' },
-      { id: 4, icon: <img src="/icons/snow.png" alt="Snowy" className="weather-icon" />, src: '/Asset/snowy-beach.mp3' },
+      { id: 1, icon: <img src={`${process.env.PUBLIC_URL}/icons/sun.png`} alt="Sunny" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/sunny-beach.mp3` },
+      { id: 2, icon: <img src={`${process.env.PUBLIC_URL}/icons/wind.png`} alt="Windy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/windy-beach.mp3` },
+      { id: 3, icon: <img src={`${process.env.PUBLIC_URL}/icons/rain.png`} alt="Rainy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/rainy-beach.mp3` },
+      { id: 4, icon: <img src={`${process.env.PUBLIC_URL}/icons/snow.png`} alt="Snowy" className="weather-icon" />, src: `${process.env.PUBLIC_URL}/Asset/snowy-beach.mp3` },
     ],
   };
 
   const tracks = sceneTracks[currentScene] || [];
   // Always use sunny version (id: 1) as default for each scene
-const defaultTrack = tracks.find(track => track.id === 1);
+  const defaultTrack = tracks.find(track => track.id === 1);
 
   const toggleDropdown = () => setDropdownVisible(!isDropdownVisible);
 
@@ -32,13 +32,13 @@ const defaultTrack = tracks.find(track => track.id === 1);
     if (audio) {
       audio.pause();
     }
-
+  
     const newAudio = new Audio(track.src);
     newAudio.addEventListener('ended', () => {
       newAudio.currentTime = 0;
       newAudio.play();
     });
-
+  
     setAudio(newAudio);
     newAudio.play();
     setIsPlaying(true);
@@ -78,7 +78,7 @@ const defaultTrack = tracks.find(track => track.id === 1);
           <div className="music-controls">
             <button className="control-button" onClick={togglePlayPause}>
               <img
-                src="/icons/pnp.png"
+                src={isPlaying ? `${process.env.PUBLIC_URL}/icons/pnp.png` : `${process.env.PUBLIC_URL}/icons/pnp.png`}
                 alt={isPlaying ? "Stop" : "Play"}
                 className="control-icon"
               />
